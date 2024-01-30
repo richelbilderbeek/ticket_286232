dde955b5236eea8ff5419ed827dee6f6bc3054f5
richelbilderbeek <richel@richelbilderbeek.nl>
2024-01-30 13:50:27 +0100

    Correct path

--- ticket_286232.sh
+++ ticket_286232.sh
@@ -7,7 +7,7 @@
 module load bioinfo-tools python3/3.11.4 wrf-python/1.3.1 
 pip3 install boldigger_cline
 python3 --version
-./.local/bin/boldigger-cline --version
+~/.local/bin/boldigger-cline --version
 
 # boldigger-cline --version
 
