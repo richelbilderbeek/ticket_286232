520e1a43c90a2617719507b62c0ca534a01987a6
richelbilderbeek <richel@richelbilderbeek.nl>
2024-01-30 13:47:37 +0100

    Install with modules loaded

--- ticket_286232.sh
+++ ticket_286232.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-#module load python3/3.11.4
-#pip3 install boldigger_cline
+
 
 # The first line of boldigger-cline uses this version:
 # #!/sw/comp/python3/3.11.4/rackham/bin/python3.11
 
 module load bioinfo-tools python3/3.11.4 wrf-python/1.3.1 
-# Tk/8.6.11-GCCcore-11.2.0
-# module load Tcl-Tk/8.6.11 Tkinter/3.9.6-GCCcore-11.2.0
-
+pip3 install boldigger_cline
 python3 --version
-
-
 ./.local/bin/boldigger-cline --version
 
 # boldigger-cline --version
