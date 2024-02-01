0ca9663897de56324023aae484ebd5e9013e2ef9
richelbilderbeek <richel@richelbilderbeek.nl>
2024-02-01 13:52:58 +0100

    Provide a solution that can work

--- ticket_286232.sh
+++ ticket_286232.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+module load bioinfo-tools
+module load wrf-python/1.3.1
 module load Tkinter/3.9.6-GCCcore-11.2.0
 pip3 install --user --force-reinstall boldigger_cline
 ~/.local/bin/boldigger-cline --version
