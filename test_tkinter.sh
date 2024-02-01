3c17946997ff1f49471b8645bbe6310a0520bce9
richelbilderbeek <richel@richelbilderbeek.nl>
2024-02-01 16:19:58 +0100

    Test python modules that have tkinter baked in

---
+++ test_tkinter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+module load python/3.3
+python -c 'import tkinter'
+module load python/3.3.1
+python -c 'import tkinter'
+module load python/3.4.3
+python -c 'import tkinter'
+module load python/3.5.0
+python -c 'import tkinter'
+module load python/3.6.0
+python -c 'import tkinter'
+module load python/3.6.8
+python -c 'import tkinter'
+module load python/3.7.2
+python -c 'import tkinter'
+module load python/3.8.7
+python -c 'import tkinter'
+module load python/3.9.5
+python -c 'import tkinter'
+module load python/3.10.8
+python -c 'import tkinter'
+module load python/3.11.4
+python -c 'import tkinter'
+module load python/3.12.1
+python -c 'import tkinter'
+
